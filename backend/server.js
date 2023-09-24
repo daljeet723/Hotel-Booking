bd52514da70319eb02c40e3b6d0e9286267a9eb0
daljeet723 <dkaur5842@gmail.com>
2023-09-24 20:20:24 +0530

    basic backend structure prepare

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+import {app} from "./app.js";
+import dotenv from "dotenv";
+import {connectDatabase} from "./config/database.js";
+
+//connect to congif files to get Environment variables
+dotenv.config({path: "./backend/config/config.env"});
+
+//calling connect databse function ..use after config..otherwise will not get process.env value
+connectDatabase();
+
+
+app.listen(process.env.PORT,()=> {
+    console.log("Server is running at port " + process.env.PORT);
+})
